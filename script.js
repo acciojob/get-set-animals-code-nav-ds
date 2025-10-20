329d84d96dd818e55d094f279117832a2a6edea5
Naveen <naveentotakura1@gmail.com>
2025-10-20 22:27:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-//complete this code
-class Animal {}
+// Base Animal class
+class Animal {
+  constructor(species) {
+    this._species = species; // store species privately
+  }
 
-class Dog extends Animal {}
+  // getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // method to make sound
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+// Cat subclass
+class Cat extends Animal {
+  // specific behavior for Cat
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Dog subclass
+class Dog extends Animal {
+  // specific behavior for Dog
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Example usage
+// const myCat = new Cat("Siamese");
+// myCat.makeSound(); // The Siamese makes a sound
+// myCat.purr();      // purr
+
+// const myDog = new Dog("Golden Retriever");
+// myDog.makeSound(); // The Golden Retriever makes a sound
+// myDog.bark();      // woof
